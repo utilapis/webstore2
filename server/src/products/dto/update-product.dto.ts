0b06c15aaebf29e30f24f02ab6854c6fb7874005
Rodrigo Ibanez <rsibanez89@gmail.com>
2021-11-06 01:45:44 +1100

    02 - Configura el back-end de tu tienda online 2.0 con NestJs.

---
+++ server/src/products/dto/update-product.dto.ts
@@ -0,0 +1,10 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateProductDto } from './create-product.dto';
+
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  stock: number;
+}
